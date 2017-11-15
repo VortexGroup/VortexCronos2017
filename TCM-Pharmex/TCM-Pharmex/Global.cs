f95887c4dc3ae5e8dd391247dd69854e23a0729b
VortexGroup <30530806+VortexGroup@users.noreply.github.com>
2017-11-15 17:25:40 -0200

    Adding The Config and Global Class

---
+++ TCM-Pharmex/TCM-Pharmex/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework.Forms;
+
+namespace TCM_Pharmex
+{
+    class Global
+    {
+        internal static int idi = 0;
+        internal static int fs = 0;
+        internal static MetroForm Login = new FrmLogin();
+        internal static MetroForm Config = new FrmConfig();
+    }
+}
